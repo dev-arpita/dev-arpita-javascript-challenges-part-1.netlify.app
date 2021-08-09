2c93044474966c2a98b35823b227c4c47298f5de
Arpita Roy <arpita.roy.workar@gmail.com>
2021-08-09 23:46:45 +0530

    calculator challenges

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-// Call the correct function when the user clicks on one of the buttons
-// Perform the given calculation using num1 and num2
-// Render the result of the calculation in the paragraph with id="sum-el"
-
-// E.g. if the user clicks on the "Plus" button, you should render
-// "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
-
 
 let num1 = 8
 let num2 = 2
@@ -13,20 +6,20 @@ document.getElementById("num2-el").textContent = num2
 let sumEl = document.getElementById("sum-el");
 // Create four functions: add(), subtract(), divide(), multiply()
 function add() {
-    console.log(num1 + num2);
-    sumEl.textContent += num1 + num2;
+    let result = (num1 + num2);
+    sumEl.textContent = "sum: " + result;
 }
 function subtract() {
-    console.log(num1 - num2);
-    sumEl.textContent += num1 - num2;
+    let result = (num1 - num2);
+    sumEl.textContent = "sum: " +result;
 }
 function divide() {
     console.log(num1 / num2);
-    sumEl.textContent += num1 / num2;
+    sumEl.textContent ="sum: " + num1 / num2;
 }
 function multiply() {
     console.log(num1 * num2);
-    sumEl.textContent += num1 * num2;
+    sumEl.textContent = "sum: " + num1 * num2;
 }
 
 
