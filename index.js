f39d2f76d9534881494693b139588bcefe7d9dc0
Arpita Roy <arpita.roy.workar@gmail.com>
2021-08-09 22:33:05 +0530

    concatenate strings in a function

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-// Create two variables, firstName and lastName
-let firstName = "Arpita";
-let lastName = "Roy";
-// Concatenate the two variables into a third variable called fullName
-let fullName = firstName +" " + lastName;
-console.log(fullName);
-// Log fullName to the console
+let name = "Linda"
+let greeting = "Hi there"
+
+// Create a function that logs out "Hi there, Linda!" when called
+
+function greetLinda() {
+    console.log(greeting + ", " + name + "!")
+}
+
+greetLinda()
