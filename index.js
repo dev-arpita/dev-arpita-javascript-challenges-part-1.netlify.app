11a0b0d721a940f10176cd75a4cf1b0ddd360f6c
Arpita Roy <arpita.roy.workar@gmail.com>
2021-08-09 22:22:08 +0530

    variables practice

---
+++ index.js
@@ -0,0 +1,7 @@
+// Create two variables, firstName and lastName
+let firstName = "Arpita";
+let lastName = "Roy";
+// Concatenate the two variables into a third variable called fullName
+let fullName = firstName +" " + lastName;
+console.log(fullName);
+// Log fullName to the console
