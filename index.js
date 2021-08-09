3efad445c6a028c710fd7b23a04f755221d7cf19
Arpita Roy <arpita.roy.workar@gmail.com>
2021-08-09 23:13:19 +0530

    rendering an error meaaage

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-console.log(11 + 7) //  18
-console.log(6 + "5") // "65"
-console.log("My points: " + 5 + 9) // "My points: 59"
-console.log(2 + 2) // 4
-console.log("11" + "14") //  "1114"
+let errorMessage = document.getElementById("error");
+// When the user clicks the purchase button, render out
+function purchase() {
+    console.log("Something went wrong, please try again");
+errorMessage.textContent = "Something went wrong, please try again"
+}
+// "Something went wrong, please try again" in the paragraph
+// that has the id="error".
